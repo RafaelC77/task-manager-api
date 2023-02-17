0dc9fa8c907937627bb26379a662228025a26cd2
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2023-02-17 15:46:07 -0300

    feat: add a json object to the request body

--- src/server.js
+++ src/server.js
@@ -3,12 +3,26 @@ import { randomUUID } from "node:crypto";
 
 const tasks = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
   if (req.method === "POST" && req.url === "/task") {
+    const { title, description } = req.body;
+
     const task = {
       id: randomUUID(),
-      title: "Título da Tarefa",
-      description: "Descrição da tarefa.",
+      title,
+      description,
       completed_at: null,
       created_at: new Date(),
       updated_at: null,
